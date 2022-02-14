4703cd936321a3e83b2f36a2e621bb9ab984ea0f
johndovern <92282293+johndovern@users.noreply.github.com>
2022-02-13 17:36:03 -0800

    fix formatting

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = {
-						"FantasqueSansMono Nerd Font Mono:size=14:antialias=true:autohint=true",
-						"JoyPixels:pixelsize=14:antialias=true:autohint=true"
+            "monospace:size=14:antialias=true:autohint=true",
+            "JoyPixels:pixelsize=14:antialias=true:autohint=true"
 };
 static char normbgcolor[]           = "#212121";
 static char normbordercolor[]       = "#212121";
