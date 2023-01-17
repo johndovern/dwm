204ff9699cfeac164443903c2c8641d09b645e48
johndovern <92282293+johndovern@users.noreply.github.com>
2023-01-17 10:14:11 -0800

    Remove blw var. Calculating the value when needed

--- dwm.c
+++ dwm.c
@@ -309,7 +309,7 @@ static int dwmblockssig;
 pid_t dwmblockspid = 0;
 static int screen;
 static int sw, sh;           /* X display screen geometry width, height */
-static int bh, blw = 0;      /* bar geometry */
+static int bh;               /* bar height */
 static int lrpad;            /* sum of left and right padding for text */
 static int (*xerrorxlib)(Display *, XErrorEvent *);
 static unsigned int numlockmask = 0;
@@ -591,7 +591,7 @@ buttonpress(XEvent *e)
 		if (i < LENGTH(tags)) {
 			click = ClkTagBar;
 			arg.ui = 1 << i;
-		} else if (ev->x < x + blw)
+		} else if (ev->x < x + TEXTW(selmon->ltsymbol))
 			click = ClkLtSymbol;
 		else if (ev->x > (x = selmon->ww - TEXTW(stext) + lrpad)) {
 			click = ClkStatusText;
@@ -990,7 +990,7 @@ drawbar(Monitor *m)
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "D [%d/%d]", s, a);
 	}
 
-	w = blw = TEXTW(m->ltsymbol);
+	w = TEXTW(m->ltsymbol);
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
@@ -1798,7 +1798,7 @@ run(void)
 void
 runAutostart(void) {
 	system("setsid -f restart-redshift");
-	system("killall -q emacs; emacs --daemon &");
+	/* system("killall -q emacs; emacs --daemon &"); */
     system("killall -q xdg-desktop-portal-kde; /usr/lib/xdg-desktop-portal-kde &");
 	system("killall -q dwmblocks; dwmblocks &");
 	system("killall -q wkx; wkx --daemon &");
