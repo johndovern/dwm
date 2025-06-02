c54a53c7ea166bfdd150f071ee75f2c9303ff806
johndovern <92282293+johndovern@users.noreply.github.com>
2025-06-02 16:45:20 -0700

    upstream: sync drw.{c,h} from dmenu

--- dwm.c
+++ dwm.c
@@ -59,7 +59,6 @@
 #define ISVISIBLEONTAG(C, T)    ((C->tags & T))
 #define ISVISIBLE(C)            ISVISIBLEONTAG(C, C->mon->tagset[C->mon->seltags] || C->issticky)
 #define PREVSEL                 3000
-#define LENGTH(X)               (sizeof X / sizeof X[0])
 #define MOUSEMASK               (BUTTONMASK|PointerMotionMask)
 #define MOD(N,M)                ((N)%(M) < 0 ? (N)%(M) + (M) : (N)%(M))
 #define WIDTH(X)                ((X)->w + 2 * (X)->bw)
